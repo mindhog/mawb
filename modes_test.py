dd16ad009485aefa9a7a254889064fbd9729d56e
Michael Muller <mmuller@enduden.com>
2017-04-20 06:35:14 -0400

    Added JackRoute.

--- modes_test.py
+++ modes_test.py
@@ -1,7 +1,8 @@
 
+from collections import namedtuple
 from unittest import main, TestCase
 from midi import ControlChange, ProgramChange
-from modes import MidiRoute, MidiState, Route, Routing, StateVec
+from modes import JackRoute, MidiRoute, MidiState, RouteImpl, Routing, StateVec
 
 class FakeSequencer(object):
 
@@ -29,6 +30,24 @@ class FakeSequencer(object):
 
     def connect(self, port1, port2):
         self.events.append('connect %s, %s' % (port1, port2))
+
+class FakeJack(object):
+
+    class Port(object):
+        def __init__(self, name):
+            self.name = name
+
+    def __init__(self):
+        self.events = []
+
+    def get_all_connections(self, port):
+        return [self.Port('x'), self.Port('y')]
+
+    def connect(self, port1, port2):
+        self.events.append('connect %s, %s' % (port1, port2))
+
+    def disconnect(self, port1, port2):
+        self.events.append('disconnect %s, %s' % (port1, port2))
 
 class StateVecTest(TestCase):
 
@@ -71,31 +90,16 @@ class StateVecTest(TestCase):
                           ]
                          )
 
-class FakeRoute(Route):
-
-    def __init__(self, src, dst):
-        self.src = src
-        self.dst = dst
+class FakeRoute(RouteImpl):
 
     def getCurrentOutbounds(self, context):
         return ['foo', 'bar']
-
-    def getSourceKey(self):
-        return self.src
-
-    def getDestKey(self):
-        return self.dst
 
     def disconnect(self, context, destKey):
         context.collector.append('disconnect %s, %s' % (self.src, destKey))
 
     def connect(self, context):
         context.collector.append('connect %s, %s' % (self.src, self.dst))
-
-    def __eq__(self, other):
-        return isinstance(other, FakeRoute) and \
-            self.src == other.src and \
-            self.dst == other.dst
 
 class RoutingTest(TestCase):
 
@@ -117,6 +121,15 @@ class RoutingTest(TestCase):
         self.assertEqual(Routing(None, None, FakeRoute('a', 'b')),
                          Routing(None, None, FakeRoute('a', 'b')))
 
+    def testRouteImpl(self):
+        # We can use FakeRoute because it's derived from RouteImpl.
+        route = FakeRoute('a', 'b')
+        self.assertEqual(route.getSourceKey(), 'a')
+        self.assertEqual(route.getDestKey(), 'b')
+        self.assertEqual(route, FakeRoute('a', 'b'))
+        self.assertNotEqual(route, FakeRoute('c', 'd'))
+        self.assertNotEqual(route, namedtuple('Foo', 'src dst')('a', 'b'))
+
     def testMidiRoutes(self):
         route = MidiRoute('a', 'b')
         self.assertEqual(route.getCurrentOutbounds(
@@ -124,8 +137,6 @@ class RoutingTest(TestCase):
                          ),
                          ['1', '2', '3']
                          )
-        self.assertEqual(route.getSourceKey(), 'a')
-        self.assertEqual(route.getDestKey(), 'b')
 
         seq = FakeSequencer()
         route.disconnect(Routing(None, seq), 'x')
@@ -134,6 +145,23 @@ class RoutingTest(TestCase):
         seq = FakeSequencer()
         route.connect(Routing(None, seq))
         self.assertEqual(seq.events, ['connect a, b'])
+
+    def testJackRoutes(self):
+        route = JackRoute('a', 'b')
+        self.assertEqual(route.getCurrentOutbounds(
+                            Routing(FakeJack(), None)
+                         ),
+                         ['x', 'y']
+                         )
+        self.assertEqual(route.getSourceKey(), 'a')
+
+        jack = FakeJack()
+        route.disconnect(Routing(jack, None), 'x')
+        self.assertEqual(jack.events, ['disconnect a, x'])
+
+        jack = FakeJack()
+        route.connect(Routing(jack, None))
+        self.assertEqual(jack.events, ['connect a, b'])
 
 if __name__ == '__main__':
     main()
