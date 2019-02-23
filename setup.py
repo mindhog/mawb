16f8f1ceb2d1715552b96af9e22ba6a44cc30de5
Michael Muller <mmuller@enduden.com>
2019-02-23 14:44:17 -0500

    Fix installation of alsa_midi.py module.

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from distutils.core import setup, Extension
 setup(
     ext_modules = [
         Extension("_alsa_midi", sources = ['alsa_midi.i'],
-                  libraries = ['asound']
+                  libraries = ['asound'],
                   )
-    ]
+    ],
+    py_modules = ['alsa_midi'],
 )
