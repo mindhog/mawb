a5b277f23f43b5e87400ea68c704efd11983a5b1
Michael Muller <mmuller@enduden.com>
2020-07-05 15:23:24 -0400

    Allow recording events from the midi editor.

--- midiedit.py
+++ midiedit.py
@@ -3,12 +3,13 @@ from abc import abstractmethod, ABC
 from enum import Enum
 import os
 from queue import Queue
+import select
 import time
 
-from alsa_midi import SND_SEQ_OPEN_OUTPUT
+from alsa_midi import SND_SEQ_OPEN_OUTPUT, SND_SEQ_OPEN_INPUT
 from amidi import PortInfo, Sequencer
-from midi import AllSoundOff, ControlChange, NoteOn, NoteOff, Piece, \
-    PitchWheel, ProgramChange, SetTempo, Track
+from midi import AllSoundOff, ControlChange, Event as MIDIEvent, NoteOn, \
+    NoteOff, Piece, PitchWheel, ProgramChange, SetTempo, Track
 from midifile import Reader as MidiFileReader, Writer as MidiFileWriter
 from threading import Thread
 from typing import Callable, Optional, Tuple, Union
@@ -67,6 +68,11 @@ class AudioIFace(ABC):
 
         'func' should be callable from any thread, not necessarily just the UI
         thread.  It is passed the current play position (in ticks).
+        """
+
+    @abstractmethod
+    def set_event_recorder(self, func: Callable[[MIDIEvent], None]) -> None:
+        """Set a function to be called when a new event comes in during play.
         """
 
     @abstractmethod
@@ -198,6 +204,7 @@ class MidiEditor(Frame):
         if audio:
             self.__play_callback(0)
             audio.set_play_callback(self.__play_callback)
+            audio.set_event_recorder(self.get_event_recorder())
 
             self.__pos_marker = self.__canvas.create_line(x, 0, x,
                                                           128 * ROW_HEIGHT,
@@ -404,13 +411,46 @@ class MidiEditor(Frame):
         note = self.__note_from_y(event.y)
         t = self.__time_from_x(event.x)
         self.__audio.begin_note_edit(note, velocity=127)
-        id = self.__draw_new_note(note, t, t + self.__note_len)
         note_on = NoteOn(t, self.__channel, note, self.__velocity)
         note_off = NoteOff(t + self.__note_len, self.__channel, note, 0)
+        self.__add_note_pair(note_on, note_off)
+
+    def __add_note_pair(self, note_on: NoteOn, note_off: NoteOff) -> None:
+        id = self.__draw_new_note(note_on.note, note_on.time,
+                                  note_off.time)
         self.__track.add(note_on)
         self.__track.add(note_off)
         self.__note_map[id] = [note_on, note_off]
         self.__select_single(id)
+
+    def get_event_recorder(self) -> Callable[[MIDIEvent], None]:
+        """Returns a function that records midi events.
+
+        The function returned can safely be called from any thread.
+        """
+        # Keeps track of which notes from the input device have a NoteOn
+        # event that hasn't been closed.
+        note_map : Dict[int, NoteOn] = {}
+
+        def record_event(event: MIDIEvent):
+            # The internal record-event function, that actually stores the
+            # event in the track.
+            if isinstance(event, NoteOn):
+                note_map[event.note] = event
+            elif isinstance(event, NoteOff):
+                start = note_map.get(event.note)
+                if start:
+                    self.__add_note_pair(start, event)
+                    del note_map[event.note]
+            else:
+                self.__track.add(event)
+
+        def on_event(event: MIDIEvent):
+            # This is the returned record function that is safe to call in a
+            # non-ui thread.
+            self.__queue.put(lambda: record_event(event))
+
+        return on_event
 
     def __end_add_note(self, event: Event) -> None:
         self.__audio.end_note_edit()
@@ -455,6 +495,7 @@ class AlsaAudioIFace(AudioIFace):
         self.__ticks_per_sec = 48
         self.__stopped = True
         self.__callback = None
+        self.__record_event = None
         self.__thread = None
         self.__track = None
         self.__ppb = ppb
@@ -480,6 +521,9 @@ class AlsaAudioIFace(AudioIFace):
             # start time is the time when the beginning of the track started
             start_time = time.time() - self.__pos / self.__ticks_per_sec
 
+            # Get the input handler to allow us to read midi events.
+            input_handle = self.seq.getPollHandle()
+
             # 't' is current time in ticks.
             t = self.__pos
             for event in self.__track:
@@ -488,13 +532,30 @@ class AlsaAudioIFace(AudioIFace):
                 next_event_ticks = event.time
                 tps = self.__ticks_per_sec
                 while t < next_event_ticks:
-                    time.sleep(min((next_event_ticks - t) / tps,
-                                   0.25
-                                   )
-                               )
+                    handles = select.select([input_handle], [], [input_handle],
+                                            min((next_event_ticks - t) / tps,
+                                                0.25
+                                                )
+                                            )
                     if self.__stopped:
                         return
-                    self.__pos = t = (time.time() - start_time) * tps
+
+                    self.__pos = t = int((time.time() - start_time) * tps)
+
+                    # Process all input events.
+                    if handles[0]:
+                        while self.seq.hasEvent():
+                            ev = self.seq.getEvent(t)
+                            if isinstance(ev,
+                                          (NoteOn, NoteOff, ControlChange,
+                                           ProgramChange, PitchWheel)
+                                          ):
+                                self.seq.sendEvent(ev, self.port)
+                                if self.__record_event:
+                                    self.__record_event(ev)
+                            else:
+                                print(f'got unknonwn event {ev}')
+
                     if self.__callback:
                         self.__callback(t)
                 if isinstance(event, (NoteOn, NoteOff, ControlChange,
@@ -518,6 +579,9 @@ class AlsaAudioIFace(AudioIFace):
 
     def set_play_callback(self, func: Callable[[int], None]) -> None:
         self.__callback = func
+
+    def set_event_recorder(self, func: Callable[[MIDIEvent], None]) -> None:
+        self.__record_event = func
 
     def set_pos(self, pos: int) -> None:
         self.__pos = pos
@@ -558,8 +622,9 @@ if __name__ == '__main__':
         MidiFileWriter(open(filename, 'wb')).writePiece(piece)
     tk = Tk()
     tk.bind('<F2>', save)
-    seq = Sequencer(SND_SEQ_OPEN_OUTPUT, 0, name='midiedit')
+    seq = Sequencer(SND_SEQ_OPEN_INPUT | SND_SEQ_OPEN_OUTPUT, 0, name='midiedit')
     port = seq.createOutputPort('out')
+    seq.createInputPort('in')
     print(f'ppqn = {track.ppqn}')
     win = MidiEditor(tk, track, AlsaAudioIFace(seq, port, track.ppqn))
     win.mainloop()
